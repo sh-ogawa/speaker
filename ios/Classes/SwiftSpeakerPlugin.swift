e12d6d26990a017ced1de0231881a41de65030f6
sh-ogawa <syogo0417@gmail.com>
2019-11-21 23:21:42 +0900

    add: 雰囲気iOS側を実装

--- ios/Classes/SwiftSpeakerPlugin.swift
+++ ios/Classes/SwiftSpeakerPlugin.swift
@@ -1,5 +1,5 @@
 import Flutter
-import UIKit
+import AVFoundation
 
 public class SwiftOoga04SpeakerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +9,31 @@ public class SwiftOoga04SpeakerPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if call.method == "play" {
+      let arguments = call.arguments as! [String: Any]
+      guard let resourceUri = arguments["resourceUri"] as? String else {
+         result(FlutterError.init(code: "ArgumentError", message: "Required argument does not exist.", details: nil));
+         return
+      }
+
+      let soundPath = resourceUri.split(separator: ".").map { String($0) }
+
+      guard let path = Bundle.main.path(forResource: soundPath[0], ofType: soundPath[1]) else {
+        print("not found file.")
+        return
+      }
+
+      do {
+        audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+        if audioPlayer.isPlaying() {
+          audioPlayer.stop();
+        }
+
+        audioPlayer.delegate = self
+        audioPlayer.volume = 1.0
+        audioPlayer.play()
+      } catch {
+      }
+    }
   }
 }
