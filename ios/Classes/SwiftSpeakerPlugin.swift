d76e54b018f3a64173d9a6bf984eb4b06648b766
sh-ogawa <syogo0417@gmail.com>
2019-11-22 11:25:29 +0900

    add: iOSでも音を鳴るようにした

--- ios/Classes/SwiftSpeakerPlugin.swift
+++ ios/Classes/SwiftSpeakerPlugin.swift
@@ -1,38 +1,32 @@
 import Flutter
+import AVKit
 import AVFoundation
 
-public class SwiftOoga04SpeakerPlugin: NSObject, FlutterPlugin {
+public class SwiftOoga04SpeakerPlugin: NSObject, FlutterPlugin, AVAudioPlayerDelegate {
+  let player = AVPlayer()
+
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ooga04_speaker", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "ooga04/speaker", binaryMessenger: registrar.messenger())
     let instance = SwiftOoga04SpeakerPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if call.method == "play" {
-      let arguments = call.arguments as! [String: Any]
-      guard let resourceUri = arguments["resourceUri"] as? String else {
-         result(FlutterError.init(code: "ArgumentError", message: "Required argument does not exist.", details: nil));
-         return
-      }
 
-      let soundPath = resourceUri.split(separator: ".").map { String($0) }
-
-      guard let path = Bundle.main.path(forResource: soundPath[0], ofType: soundPath[1]) else {
-        print("not found file.")
-        return
-      }
-
-      do {
-        audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-        if audioPlayer.isPlaying() {
-          audioPlayer.stop();
+    let arguments = call.arguments as! [String: Any]
+    guard let resourceUri = arguments["resourceUri"] as? String else {
+      result(FlutterError.init(code: "ArgumentError", message: "Required argument does not exist.", details: nil));
+      return
+    }
+    if let encodeUrl = resourceUri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+      if let url = URL(string: encodeUrl) {
+        let playerItem = AVPlayerItem(url: url)
+          player.replaceCurrentItem(with: playerItem)
+          player.volume = 1.0
+          player.play()
+          result("request success");
         }
-
-        audioPlayer.delegate = self
-        audioPlayer.volume = 1.0
-        audioPlayer.play()
-      } catch {
       }
     }
   }
