b96d7c6cefa0b9ca4cc883a929539bc72f164638
sh-ogawa <syogo0417@gmail.com>
2019-11-27 18:28:40 +0900

    add: swift側の雛形だけ足しといた

--- ios/Classes/SwiftSpeakerPlugin.swift
+++ ios/Classes/SwiftSpeakerPlugin.swift
@@ -14,20 +14,24 @@ public class SwiftOoga04SpeakerPlugin: NSObject, FlutterPlugin, AVAudioPlayerDel
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if call.method == "play" {
 
-    let arguments = call.arguments as! [String: Any]
-    guard let resourceUri = arguments["resourceUri"] as? String else {
-      result(FlutterError.init(code: "ArgumentError", message: "Required argument does not exist.", details: nil));
-      return
-    }
-    if let encodeUrl = resourceUri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
-      if let url = URL(string: encodeUrl) {
-        let playerItem = AVPlayerItem(url: url)
+      let arguments = call.arguments as! [String: Any]
+      guard let resourceUri = arguments["resourceUri"] as? String else {
+        result(FlutterError.init(code: "ArgumentError", message: "Required argument does not exist.", details: nil));
+        return
+      }
+      if let encodeUrl = resourceUri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+        if let url = URL(string: encodeUrl) {
+          let playerItem = AVPlayerItem(url: url)
           player.replaceCurrentItem(with: playerItem)
           player.volume = 1.0
           player.play()
           result("request success");
         }
       }
+    } else if call.method == "plays" {
+
+    } else {
+
     }
   }
 }
