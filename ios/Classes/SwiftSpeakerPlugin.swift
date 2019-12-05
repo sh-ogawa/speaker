31399eb598130e633a3458e42b06b8eb505e313e
sh-ogawa <syogo0417@gmail.com>
2019-12-05 11:05:49 +0900

    add: ios側に再生の終了を検知してdart側のコードを呼び出す処理を追加

--- ios/Classes/SwiftSpeakerPlugin.swift
+++ ios/Classes/SwiftSpeakerPlugin.swift
@@ -3,35 +3,46 @@ import AVKit
 import AVFoundation
 
 public class SwiftOoga04SpeakerPlugin: NSObject, FlutterPlugin, AVAudioPlayerDelegate {
-  let player = AVPlayer()
+    let player = AVPlayer()
+    var resultPlay:FlutterResult?
+    private var channel: FlutterMethodChannel!
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ooga04/speaker", binaryMessenger: registrar.messenger())
-    let instance = SwiftOoga04SpeakerPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if call.method == "play" {
-
-      let arguments = call.arguments as! [String: Any]
-      guard let resourceUri = arguments["resourceUri"] as? String else {
-        result(FlutterError.init(code: "ArgumentError", message: "Required argument does not exist.", details: nil));
-        return
-      }
-      if let encodeUrl = resourceUri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
-        if let url = URL(string: encodeUrl) {
-          let playerItem = AVPlayerItem(url: url)
-          player.replaceCurrentItem(with: playerItem)
-          player.volume = 1.0
-          player.play()
-          result("request success");
-        }
-      }
-    } else if call.method == "plays" {
-
-    } else {
-
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = SwiftOoga04SpeakerPlugin(registrar: registrar, messenger: registrar.messenger())
+        registrar.addApplicationDelegate(instance)
     }
-  }
+
+    init(registrar: (NSObjectProtocol & FlutterPluginRegistrar)!, messenger: (NSObjectProtocol & FlutterBinaryMessenger)!) {
+        super.init()
+
+        channel = FlutterMethodChannel(name: "ooga04/speaker", binaryMessenger: registrar.messenger())
+        registrar?.addMethodCallDelegate(self, channel: channel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "play" {
+
+            let arguments = call.arguments as! [String: Any]
+            guard let resourceUri = arguments["resourceUri"] as? String else {
+                result(FlutterError.init(code: "ArgumentError", message: "Required argument does not exist.", details: nil));
+                return
+            }
+
+            if let encodeUrl = resourceUri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                if let url = URL(string: encodeUrl) {
+                    resultPlay = result
+                    let playerItem = AVPlayerItem(url: url)
+                    NotificationCenter.default.addObserver(self, selector: #selector(self.playerDidFinishPlaying(sender:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: playerItem)
+                    player.replaceCurrentItem(with: playerItem)
+                    player.volume = 1.0
+                    player.play()
+                    result("request success");
+                }
+            }
+        }
+    }
+
+    @objc func playerDidFinishPlaying(sender: Notification) {
+        channel.invokeMethod("onSpeakEnd", arguments: [])
+    }
 }
